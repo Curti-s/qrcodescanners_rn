46c27d983f9a10e940ad5e96d716c8d1566f0585
k1r1m1 <kirimi@paygoenergy.co>
2021-10-07 16:42:58 +0300

    Visioncamera screen displays, but no barcode detection, yet

--- src/components/cameras/VisionCameraScreen.js
+++ src/components/cameras/VisionCameraScreen.js
@@ -1,13 +1,86 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useCallback,
+} from 'react';
 import {
   View,
+  StyleSheet,
   Text,
+  ActivityIndicator,
+  Pressable,
 } from 'react-native';
+import  {
+  Camera,
+  useCameraDevices,
+} from 'react-native-vision-camera';
+import {
+  Button,
+} from 'react-native-paper';
 
 export default ({ navigation }) => {
+  const isMounted = useRef(false);
+  const [cameraPermission, setCameraPermission] = useState('not-determined');
+  const [isActive, setIsActive] = useState(false);
+  const [flash, setFlash] = useState('off');
+
+  useEffect(() => {
+    // track mounted state
+    isMounted.current = true;
+    setIsActive(true);
+
+    return () => {
+      isMounted.current = false;
+      setIsActive(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if(isMounted.current) {
+        try {
+          const isCameraPermitted = await Camera.getCameraPermissionStatus();
+
+          if(isCameraPermitted !== 'authorized') {
+            try {
+              const permissionRequest = await Camera.requestCameraPermission();
+              setCameraPermission(permissionRequest);
+            } catch(err) {
+              console.error(`request camera permission failed: ${JSON.stringify(err)}`);
+            }
+          }
+        } catch(err) {
+          console.error(`get camera permission failed: ${JSON.stringify(err)}`);
+        }
+      }
+    })();
+  },[cameraPermission]);
+
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  const toggleFlash = useCallback(() => setFlash(f => (f === 'off' ? 'on' : 'off')));
+
+  if(device == null) return <ActivityIndicator />;
+
   return (
-    <View style={{ flex:1 }}>
-      <Text>vision camera screen</Text>
+    <View style={styles.container}>
+      <Camera style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={isActive}
+        torch={flash}
+      />
+      <View style={{ backgroundColor:'transparent', alignItems:'flex-end' }}>
+        <Button icon="flash" color="white" mode="text" onPress={toggleFlash}>Flash</Button>
+      </View>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor:'black',
+  }
+});
