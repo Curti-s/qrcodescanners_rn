a27a47c5aed9685cc4246293a44912cf17f4a13a
k1r1m1 <kirimi@paygoenergy.co>
2021-10-07 15:13:07 +0300

    install react-native-paper, react-native-vector-icons

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import {
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 import Section from './src/components/Sections';
 import ScreenWrapper from './src/components/ScreenWrapper';
@@ -40,16 +41,18 @@ const HomeScreen: () => Node = ({ navigation }) => {
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="home">
-        <Stack.Screen name="home" component={HomeScreen} />
-        <Stack.Screen name="singleScanScreen" component={SingleScanScreen} />
-        <Stack.Screen name="bulkScanScreen" component={BulkScanScreen} />
-        <Stack.Screen name="rnCameraScreen" component={ReactNativeCameraScreen} />
-        <Stack.Screen name="hmsScreen" component={HmsScankitScreen} />
-        <Stack.Screen name="visionCameraScreen" component={VisionCameraScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="home">
+          <Stack.Screen name="home" component={HomeScreen} />
+          <Stack.Screen name="singleScanScreen" component={SingleScanScreen} />
+          <Stack.Screen name="bulkScanScreen" component={BulkScanScreen} />
+          <Stack.Screen name="rnCameraScreen" component={ReactNativeCameraScreen} />
+          <Stack.Screen name="hmsScreen" component={HmsScankitScreen} />
+          <Stack.Screen name="visionCameraScreen" component={VisionCameraScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
