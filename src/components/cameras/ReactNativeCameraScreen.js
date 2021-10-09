2877386071d39557450e6270f9383364407e0308
k1r1m1 <kirimi@paygoenergy.co>
2021-10-09 09:05:33 +0300

    ReactNativeCameraScreen: update flash button appearance

--- src/components/cameras/ReactNativeCameraScreen.js
+++ src/components/cameras/ReactNativeCameraScreen.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import {
   View,
-  Text,
   StyleSheet,
-  Pressable,
 } from 'react-native';
+import {
+  Button,
+} from 'react-native-paper';
 import { RNCamera } from 'react-native-camera';
 
 export default class ReactNativeCameraScreen extends React.PureComponent {
@@ -12,7 +13,7 @@ export default class ReactNativeCameraScreen extends React.PureComponent {
     flashMode: RNCamera.Constants.FlashMode.off,
   }
 
-  toggleFlashMode = () => this.setState({ flashMode: this.state.flashMode ===  RNCamera.Constants.FlashMode.off ?
+  toggleFlash = () => this.setState({ flashMode: this.state.flashMode ===  RNCamera.Constants.FlashMode.off ?
       RNCamera.Constants.FlashMode.torch : RNCamera.Constants.FlashMode.off });
   
   render() {
@@ -24,7 +25,7 @@ export default class ReactNativeCameraScreen extends React.PureComponent {
     return (
       <View style={styles.container}>
         <RNCamera
-          style={styles.preview}
+          style={StyleSheet.absoluteFill}
           type={RNCamera.Constants.Type.back}
           flashMode={flashMode}
           androidCameraPermissionOptions={{
@@ -47,11 +48,8 @@ export default class ReactNativeCameraScreen extends React.PureComponent {
           googleVisionBarcodeMode={RNCamera.Constants.GoogleVisionBarcodeDetection.BarcodeMode.ALTERNATE}
           captureAudio={false}
         />
-        <View style={styles.toggleBtnArea}>
-          <Pressable
-            style={[styles.btn, { alignSelf: 'flex-end' }]} onPress={this.toggleFlashMode}>
-            <Text style={[styles.txt]}>Flash: {this.state.flashMode === 0 ? 'off' : 'on'}</Text>
-          </Pressable>
+        <View style={{ backgroundColor:'transparent', alignItems:'flex-end' }}>
+          <Button icon="flash" color="white" mode="text" onPress={this.toggleFlash}>Flash</Button>
         </View>
       </View>
     )
@@ -63,30 +61,5 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: 'black',
-  },
-  preview: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  toggleBtnArea: {
-    flex:0.1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-  },
-  btn: {
-    flex: 0.3,
-    height: 28,
-    borderRadius: 8,
-    borderColor: 'white',
-    borderWidth: 1,
-    marginVertical: 5,
-    marginHorizontal: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  txt: {
-    color: 'white',
-    fontSize: 12,
   },
 });
